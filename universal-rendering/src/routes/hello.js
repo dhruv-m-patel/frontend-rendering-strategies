e7aa72dc878b27ac4b33c011a3761d3e2f45e34a
Dhruv Patel <dpatel@gasbuddy.com>
2019-10-30 09:44:40 -0600

    Add console logs to effectively showcase universal rendering example

--- universal-rendering/src/routes/hello.js
+++ universal-rendering/src/routes/hello.js
@@ -1,5 +1,6 @@
 export default async function hello(router) {
   router.get('/', async (req, res, next) => {
+    console.log('On hello page');
     next();
   });
 }
--- universal-rendering/src/routes/index.js
+++ universal-rendering/src/routes/index.js
@@ -1,5 +1,6 @@
 export default async function index(router) {
   router.get('/', async (req, res, next) => {
+    console.log('On index page');
     next();
   });
 }
